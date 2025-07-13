121ff49c50094f7abbbd28f226cbb5384c6264f1
Amanda Gabriela <amandagabriela056@gmail.com>
2025-07-13 11:29:10 -0300

    feat: componet de botão

---
+++ front/components/Button.tsx
@@ -0,0 +1,36 @@
+import { Colors } from "@/constants/Colors";
+import React from "react";
+import {
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  TouchableOpacityProps,
+} from "react-native";
+
+interface AppButtonProps extends TouchableOpacityProps {
+  label: string;
+}
+
+export function AppButton({ label, style, ...rest }: AppButtonProps) {
+  return (
+    <TouchableOpacity style={[styles.button, style]} {...rest}>
+      <Text style={styles.label}>{label}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: Colors.primary_1,
+    paddingVertical: 10,
+    paddingHorizontal: 28,
+    textAlign: "center",
+    alignItems: "center",
+    borderRadius: 8,
+  },
+  label: {
+    color: "#fff",
+    fontSize: 12,
+    fontWeight: "bold",
+  },
+});
