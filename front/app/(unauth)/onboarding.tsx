7478c1918928734e7528a4bb9e6957b263d0a73d
Amanda Gabriela <amandagabriela056@gmail.com>
2025-07-13 11:30:48 -0300

    fix: ajuste onboarding

--- front/app/(unauth)/onboarding.tsx
+++ front/app/(unauth)/onboarding.tsx
@@ -2,10 +2,16 @@ import React from "react";
 import Onboarding from "react-native-onboarding-swiper";
 import { Image, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { Colors } from "@/constants/Colors";
-import { useRouter } from 'expo-router';
+import { useRouter } from "expo-router";
+import { AppButton } from "@/components/Button";
+interface SquareProps {
+  isLight: boolean;
+  selected: boolean;
+}
 
-export default function OnboardingScreen(){
+export default function OnboardingScreen() {
   const router = useRouter();
+
   const pages = [
     {
       backgroundColor: "#F7F7F7",
@@ -42,6 +48,27 @@ export default function OnboardingScreen(){
     },
   ];
 
+  const Square: React.FC<SquareProps> = ({ isLight, selected }) => {
+    const backgroundColor = selected
+      ? isLight
+        ? "#000"
+        : "#fff"
+      : "rgba(0, 0, 0, 0.3)";
+    return (
+      <View
+        style={{
+          width: 8,
+          height: 8,
+          marginHorizontal: 3,
+          backgroundColor,
+        }}
+      />
+    );
+  };
+
+  const Next = (props: any) => <AppButton label="Próximo" {...props} />;
+  const Done = (props: any) => <AppButton label="Concluir" {...props} />;
+
   return (
     <View style={styles.wrapper}>
       <Image
@@ -51,17 +78,19 @@ export default function OnboardingScreen(){
 
       <Onboarding
         pages={pages}
-        onSkip={() => router.replace('/profile')}
-        onDone={() => router.replace('/profile')}
+        DotComponent={Square}
+        NextButtonComponent={Next}
         showSkip={false}
-        showNext={false}
-        showDone={false}
+        DoneButtonComponent={Done}
+        onDone={() => router.replace("/profile")}
         containerStyles={styles.container}
         titleStyles={styles.title}
         subTitleStyles={styles.subtitle}
+        bottomBarHighlight={false}
+        bottomBarHeight={100}
       />
 
-      <TouchableOpacity onPress={() => router.replace('/profile')}>
+      <TouchableOpacity onPress={() => router.replace("/profile")}>
         <Text style={styles.skipText}>Pular introdução</Text>
       </TouchableOpacity>
     </View>
@@ -71,17 +100,21 @@ export default function OnboardingScreen(){
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
+    backgroundColor: "#F7F7F7",
+    /* paddingHorizontal: 15, */
   },
   logo: {
     width: 180,
     height: 60,
     resizeMode: "contain",
     alignSelf: "center",
+    marginTop: 50,
   },
   container: {
     flex: 1,
     justifyContent: "center",
-    gap: 50,
+    backgroundColor: "#F7F7F7",
+    paddingBottom: 70,
   },
   image: {
     width: 80,
@@ -102,9 +135,20 @@ const styles = StyleSheet.create({
   },
   skipText: {
     textAlign: "center",
-    marginBottom: 24,
+    marginBottom: 50,
     fontSize: 14,
     fontWeight: "500",
     color: "#555",
+  },
+  buttonWrapper: {
+    alignSelf: "center", // ⬅️ centraliza abaixo dos dots
+    paddingHorizontal: 24,
+    paddingVertical: 10,
+    borderRadius: 20,
+    marginTop: 16,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
