b8118813344508c222afc691e925cec1d0627b7a
pauloJ-r <pj74586@gmail.com>
2025-08-13 11:35:41 -0300

    feat: modify icons

--- front/services/api.ts
+++ front/services/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.18.109:3000/", // Altere para sua URL real
+  baseURL: "http://study-connect-app.vercel.app", 
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
